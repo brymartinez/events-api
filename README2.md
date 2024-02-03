5d2e757a7da2308edcb75da342ee180c8456358e
Bryan Louie Martinez <bry.martinez@gmail.com>
2024-02-03 20:48:16 +0800

    Make seeder

--- README2.md
+++ README2.md
@@ -5,5 +5,6 @@ cp .env.example .env # Edit according to `docker-compose.yml` credentials
 composer install
 php artisan key:generate
 php artisan migrate
+php artisan db:seed
 php artisan serve
 ```
---
+++ database/seeders/UsersSeeder.php
@@ -0,0 +1,25 @@
+<?php
+
+namespace Database\Seeders;
+
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use Illuminate\Database\Seeder;
+use App\Models\User;
+
+class UsersSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     */
+    public function run(): void
+    {
+        $faker = \Faker\Factory::create();
+        for ($i = 0; $i < 10; $i++) {
+            User::create([
+                'name' => $faker->name,
+                'email' => $faker->email,
+                'password' => $faker->password,
+            ]);
+        }
+    }
+}
